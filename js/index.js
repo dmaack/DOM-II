96aa1d5f87e74ed8f027bc2bb520a6b64e032d5b
dominique maack <dominiquemaack@gmail.com>
2019-09-03 17:04:40 -0600

    added more events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-/*** #1 - MOUSEOVER  + MOUSEENTER ***/
+/*** #1 - MOUSEOVER + MOUSELEAVE - ***/
 
 const imgScaleUp = document.querySelector('.intro img');
 imgScaleUp.addEventListener('mouseover' , e => {
@@ -12,4 +12,45 @@ const imgScaleDown = document.querySelector('.intro img');
 imgScaleDown.addEventListener('mouseleave' , e => {
     imgScaleDown.style.transform = 'scale(1)';
     imgScaleDown.style.transition = 'transform 0.5s';
+})
+
+
+/*** #2 - KEYDOWN - ***/
+
+
+/*** #3 - WHEEL - ***/
+
+const fbScroll = document.querySelector('.logo-heading');
+fbScroll.addEventListener('wheel' , e => {
+    fbScroll.style.fontSize = '6rem';
+    fbScroll.style.color = 'gold';
+})
+
+/*** #4 - DRAG/DROP - ***/
+
+const dragged = document.querySelector('.destination h4');
+dragged.addEventListener('drag' , e => {
+   
+} , false);
+
+dragged.addEventListener('dragstart' , e => {
+    dragged = e.target;
+    e.taget.style.opacity = .5;
+},false);
+
+
+/****  #5 CLICK ********/
+
+const bodyBgColor = document.querySelector('body');
+bodyBgColor.addEventListener('click' , e => {
+    bodyBgColor.style.backgroundColor = 'gold';
+})
+
+
+/********* #6 RESIZE WINDOW ***********/
+
+window.addEventListener('resize' , e => {
+    const changeTitle = document.querySelector('.intro h2');
+    changeTitle.textContent = "You're in the Fun Bus!";
+    changeTitle.style.color = 'red';
 })

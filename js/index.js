ae8138501cb9e08102490d378330f82b9b31f982
dominique maack <dominiquemaack@gmail.com>
2019-09-03 14:12:46 -0600

    added first Event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+/*** #1 - MOUSEOVER  + MOUSEENTER ***/
+
+const imgScaleUp = document.querySelector('.intro img');
+imgScaleUp.addEventListener('mouseover' , e => {
+    imgScaleUp.style.transform = 'scale(1.2)';
+    imgScaleUp.style.transition = 'transform 0.5s';
+})
+
+const imgScaleDown = document.querySelector('.intro img');
+imgScaleDown.addEventListener('mouseleave' , e => {
+    imgScaleDown.style.transform = 'scale(1)';
+    imgScaleDown.style.transition = 'transform 0.5s';
+})
